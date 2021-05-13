699fcc523949a2891b642a6a5a42b29b7b364444
sei-994 <s.hoshino0517basket@gmail.com>
2021-05-14 04:00:00 +0900

    create card

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,40 @@
 <template>
   <v-app>
     <v-img src="../public/images/home-main.jpg"></v-img>
-    <v-main>
-      <HelloWorld />
-    </v-main>
+    <v-container class="fill-height" fluid style="min-height: 434px">
+      <v-row class="d-flex ma-6" justify="center">
+        <v-col cols="5">
+          <v-card>
+            <v-img
+              src="../public/images/card.jpg"
+              height="500"
+              width="500"
+              class="grey darken-4"
+              contain
+            ></v-img>
+          </v-card>
+        </v-col>
+        <v-col max-width="500" cols="5">
+          <v-card>
+            <v-img
+              src="../public/images/cards.jpg"
+              height="125"
+              contain
+              class="grey darken-4"
+            ></v-img>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+    <v-main> </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld"
-
 export default {
   name: "App",
 
-  components: {
-    HelloWorld,
-  },
+  components: {},
 
   data() {
     return {
